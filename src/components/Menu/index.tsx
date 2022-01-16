3761b1ee85de1b8f4206e6e8f6b4f41a3b40d60c
Per Steen Olsen <persteen22@hotmail.com>
2022-01-16 15:48:57 +0100

    The file with the typedeclarations was moved to the same level as tsconfig

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,14 +3,6 @@
 
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import ReactDOM from "react-dom";
-
-// TEST
-// Remove the HTML tags and the statement will not pass TypeScript types checking 
-// Use the valid HTML tags and the statement will pass TypeScript types checking  
-
-import { FC } from 'react'
-const Hello: FC = () => <div style={{color: "green"}}>Hello World :-) <b> - React with TypeScript</b> served at the {new Date().toLocaleDateString()}</div>
 
 import { Home } from 'components/Home/index.tsx';
 import { About } from 'components/About/index.tsx';
